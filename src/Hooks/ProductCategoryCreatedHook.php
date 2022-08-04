72734cea19fd90ac92387023726b562e9ae68429
Seokman Hong <exceedweb@gmail.com>
2022-08-04 09:00:58 +0900

    feat: add product.sales_status

--- src/Hooks/ProductCategoryCreatedHook.php
+++ src/Hooks/ProductCategoryCreatedHook.php
@@ -2,12 +2,12 @@
 
 namespace Mxncommerce\ChannelConnector\Hooks;
 
+use App\Helpers\ChannelConnectorFacade;
 use App\Jobs\Features\SendModelChangeToRemote;
 use App\Libraries\CustomException\CustomExceptionInterface;
 use App\Models\Features\Category;
 use App\Models\Features\Product;
 use App\Models\Pivot\ProductOverridedCategory;
-use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class ProductCategoryCreatedHook {
@@ -19,6 +19,10 @@ class ProductCategoryCreatedHook {
     public function __invoke(ProductOverridedCategory $productOverridedCategory): void
     {
         $product = Product::find($productOverridedCategory->product_id);
+        if (ChannelConnectorFacade::isSalesDisabled($product)) {
+            return;
+        }
+
         if (!$product instanceof Product) {
             $error_namespace = 'mxncommerce.channel-connector::channel_connector.errors.no_product_found';
             $error = trans($error_namespace, [
